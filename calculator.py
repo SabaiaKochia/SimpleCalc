9b880efda50f5c5456b144234790f0dda45ea87d
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-02-29 10:05:06 -0500

    Update calculator.py
    Added Power

--- calculator.py
+++ calculator.py
@@ -9,24 +9,33 @@ def add_data():
     num1_val = int(num1.get())
     num2_val = int(num2.get())
     result = num1_val + num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 5, column = 1)
+    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
 def sub_data():
     num1_val = int(num1.get())
     num2_val = int(num2.get())
     result = num1_val - num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 5, column = 1)
+    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
 
 def mul_data():
     num1_val = int(num1.get())
     num2_val = int(num2.get())
     result = num1_val * num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 5, column = 1)
+    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
 
 def div_data():
     num1_val = int(num1.get())
     num2_val = int(num2.get())
     result = num1_val / num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 5, column = 1)
+    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
+def power_data():
+    num1_val = int(num1.get())
+    num2_val = int(num2.get())
+    result = 1
+    i = 0
+    while num2_val > i:
+        result = result * num1_val
+        i = i + 1
+    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
 #   declare a window
 window = Tk()
 
@@ -48,6 +57,8 @@ Button( window, text='+', command= add_data).grid(row=3,column=0,pady=1)
 Button( window, text='-', command= sub_data).grid(row=3,column=1,pady=1)
 Button( window, text='x', command= mul_data).grid(row=4,column=0,pady=1)
 Button( window, text='/', command= div_data).grid(row=4,column=1,pady=1)
+Button( window, text='^', command= power_data).grid(row=5,column=0,pady=1)
+
 #Allows the grid to adjust as it is reshaped
 
 for row in range(7):
