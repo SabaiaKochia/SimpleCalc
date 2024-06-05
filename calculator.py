956d746b8e932a61115f817dfa49cd5b3910e226
SabaiaKochia <90808407+SabaiaKochia@users.noreply.github.com>
2024-06-05 12:43:47 -0400

    Update 1.0 calculator.py
    added functions ; Clear, Enter and functional Exit

--- calculator.py
+++ calculator.py
@@ -1,50 +1,83 @@
 #   Saba Kochadze
 #   Dec 28, 2023
+#   Update 1.0 June 5, 2024
 #   Simple Calculator
 
 
+
+from ctypes.wintypes import RGB
 from tkinter import *
+from tkinter.tix import INTEGER
+from turtle import color
+
+
 
 def add_data():
+    global result 
+    
     num1_val = int(num1.get())
     num2_val = int(num2.get())
+    
     result = num1_val + num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
+    
 def sub_data():
+    global result
     num1_val = int(num1.get())
     num2_val = int(num2.get())
+    
     result = num1_val - num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
+
 
 def mul_data():
+    global result
     num1_val = int(num1.get())
     num2_val = int(num2.get())
+    
     result = num1_val * num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
-
+    
 def div_data():
+    global result
     num1_val = int(num1.get())
     num2_val = int(num2.get())
+    
     result = num1_val / num2_val
-    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
+    
 def power_data():
+    global result
     num1_val = int(num1.get())
     num2_val = int(num2.get())
+    
     result = 1
+    
     i = 0
     while num2_val > i:
+        
         result = result * num1_val
+        
         i = i + 1
-    Label(window, text = 'Result' + str(result)).grid(row = 6, column = 1)
+    
+def clear():
+
+    num1.delete(0, END)
+    num1.insert(0, "")
+    num2.delete(0, END)
+    num2.insert(0, "")
+    
+
+def calculate():
+    
+    lab1 = Label(window, text = 'Result' + str(result)).grid(row = i, column = 1)
+    
+    
 #   declare a window
 window = Tk()
 
 #   Title + Shape
 window.title("Simple Calculator")
 window.geometry()
-
-Label(window, text="NUM 1").grid(row=0)
-Label(window, text="NUM 2").grid(row=1)
+  
+lab1 = Label(window, text="NUM 1").grid(row=0)
+lab2 = Label(window, text="NUM 2").grid(row=1)
 
 num1 = Entry(window)
 num2 = Entry(window)
@@ -52,7 +85,9 @@ num2 = Entry(window)
 num1.grid(row=0, column=1)
 num2.grid(row = 1, column = 1)
 
-Button( window, text='Exit', command=window.quit ).grid( row=3, column=3, pady=4 )
+Button( window, text='EXIT', command=window.quit).grid( row=5, column=3, pady=4)
+Button( window, text='ENTER', command= calculate).grid( row=5, column=1, pady=1)
+Button( window, text='CLEAR', command= clear).grid( row=3, column=3, pady=4)
 Button( window, text='+', command= add_data).grid(row=3,column=0,pady=1)
 Button( window, text='-', command= sub_data).grid(row=3,column=1,pady=1)
 Button( window, text='x', command= mul_data).grid(row=4,column=0,pady=1)
